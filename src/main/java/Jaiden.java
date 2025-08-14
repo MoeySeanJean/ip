ea8ca2f909082bdabb2883bdef59d36edf367d28
MoeySeanJean <122952773+MoeySeanJean@users.noreply.github.com>
2025-08-14 22:31:17 +0800

    Echo

--- src/main/java/Jaiden.java
+++ src/main/java/Jaiden.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Jaiden {
     public static void main(String[] args) {
-        String msg = "____________________________________________________________\n"
-                + " Hello! I'm Jaiden\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(msg);
+        Scanner scanner = new Scanner(System.in);
+        String greet = "    ____________________________________________________________\n"
+                + "     Hello! I'm Jaiden\n"
+                + "     What can I do for you?\n"
+                + "    ____________________________________________________________\n";
+        String exit = "    ____________________________________________________________\n"
+                + "     Bye. Hope to see you again soon!\n"
+                + "    ____________________________________________________________\n";
+        System.out.println(greet);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            String msg = "    ____________________________________________________________\n"
+                    + "     " + input + "\n"
+                    + "    ____________________________________________________________\n";
+            System.out.println(msg);
+            input = scanner.nextLine();
+        }
+        System.out.println(exit);
+        scanner.close();
     }
 }
