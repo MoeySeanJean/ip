48ffa0931e7ba96ae0bdabbc4c53901e233b7aed
MoeySeanJean <122952773+MoeySeanJean@users.noreply.github.com>
2025-08-14 21:31:56 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Jaiden.java
@@ -0,0 +1,11 @@
+public class Jaiden {
+    public static void main(String[] args) {
+        String msg = "____________________________________________________________\n"
+                + " Hello! I'm Jaiden\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(msg);
+    }
+}
